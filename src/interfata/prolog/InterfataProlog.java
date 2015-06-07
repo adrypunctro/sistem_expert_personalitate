583c7a3a63d14da522ba52d2af239a47bea8c346
Pirill <pirill_poveniy@yahoo.com>
2015-06-07 19:11:41 +0300

    Test

---
+++ src/interfata/prolog/InterfataProlog.java
@@ -0,0 +1,15 @@
+package interfata.prolog;
+
+public class InterfataProlog {
+    //Portul prin care se face legatura cu programul
+    static final int PORT = 9876;
+    
+    public static void main(String[] args) {
+        
+        //Creaza o instanta a clasei ConexiuneProlog
+        ConexiuneProlog cxp;
+        
+        
+    }
+    
+}
